62eecd4b9266f91cd22fd5412f6eb611956cd5e4
gitlost <burmartke@gmail.com>
2017-06-18 10:33:51 +0100

    Add separate travis builds for opcache.save_comments.

--- ci/test.sh
+++ ci/test.sh
@@ -11,9 +11,9 @@ vendor/bin/phpunit
 BEHAT_TAGS=$(php ci/behat-tags.php)
 
 # Run the functional tests
-vendor/bin/behat --format progress $BEHAT_TAGS --strict
-
-# Run the functional tests with opcache.save_comments disabled.
-if [[ $TRAVIS_PHP_VERSION = '7.1' && $WP_VERSION = 'latest' ]]; then
+if [[ -n "$SAVE_COMMENTS_DISABLED" ]]; then
+	# Run the functional tests with opcache.save_comments disabled.
 	WP_CLI_PHP_ARGS='-dopcache.enable_cli=1 -dopcache.save_comments=0' vendor/bin/behat --format progress "$BEHAT_TAGS&&~@require-opcache-save-comments" --strict
+else
+	vendor/bin/behat --format progress $BEHAT_TAGS --strict
 fi
