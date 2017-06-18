a483fc9815a9d4a1f73e0c13b715ced3d4007362
gitlost <burmartke@gmail.com>
2017-06-18 11:37:34 +0100

    Perm travis matrix. Use Symfony Filesystem::remove. Try --format=failed.

--- ci/test.sh
+++ ci/test.sh
@@ -13,7 +13,7 @@ BEHAT_TAGS=$(php ci/behat-tags.php)
 # Run the functional tests
 if [[ -n "$SAVE_COMMENTS_DISABLED" ]]; then
 	# Run the functional tests with opcache.save_comments disabled.
-	WP_CLI_PHP_ARGS='-dopcache.enable_cli=1 -dopcache.save_comments=0' vendor/bin/behat --format progress "$BEHAT_TAGS&&~@require-opcache-save-comments" --strict
+	time WP_CLI_PHP_ARGS='-dopcache.enable_cli=1 -dopcache.save_comments=0' vendor/bin/behat --format failed "$BEHAT_TAGS&&~@require-opcache-save-comments" --strict
 else
 	vendor/bin/behat --format progress $BEHAT_TAGS --strict
 fi
