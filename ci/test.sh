0f0926ce498f31905303f85dbf64e5acd786cf6a
gitlost <burmartke@gmail.com>
2017-07-13 15:48:15 +0100

    Allow splitting of running commands.

--- ci/test.sh
+++ ci/test.sh
@@ -10,13 +10,19 @@ vendor/bin/phpunit
 
 BEHAT_TAGS=$(php ci/behat-tags.php)
 
-if [[ $TEST_COMMANDS -ne 1 ]]; then
+if [[ $TEST_COMMANDS -eq 0 || $((TEST_COMMANDS & 1)) > 0 ]]; then
 	# Run the functional tests
 	vendor/bin/behat --format progress $BEHAT_TAGS --strict
 fi
 
-if [[ $TEST_COMMANDS -eq 1 || $TEST_COMMANDS -eq 2 ]]; then
-	for R in vendor/wp-cli/*-command; do
+if [[ $((TEST_COMMANDS & 2)) > 0 ]]; then
+	for R in vendor/wp-cli/[a-h]*-command; do
+		BEHAT_TAGS=$(cd $R && php ../../../ci/behat-tags.php); vendor/bin/behat --format progress $BEHAT_TAGS --strict $R/features
+	done
+fi
+
+if [[ $((TEST_COMMANDS & 4)) > 0 ]]; then
+	for R in vendor/wp-cli/[i-z]*-command; do
 		BEHAT_TAGS=$(cd $R && php ../../../ci/behat-tags.php); vendor/bin/behat --format progress $BEHAT_TAGS --strict $R/features
 	done
 fi
